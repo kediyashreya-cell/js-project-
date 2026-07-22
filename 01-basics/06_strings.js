1b4b23dd80f5d02f7f71e7190e05e6fc69259a9a
kediyashreya-cell <kediyashreya@gmail.com>
2026-07-22 18:35:49 +0000

    06_strings

---
+++ 01-basics/06_strings.js
@@ -0,0 +1,39 @@
+const name = "shreya"
+const repocount = 50
+//console.log(name + repocount + "value");
+//console.log(`Hello my name is ${name} and my repo count is ${repocount} `);
+
+
+const gameName = new String('Shreya-kedia')
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('k'));
+ //const newString = gameName.substring(0,5)
+ //console.log(newString);
+
+ const anotherString = gameName.slice(-8, 4)
+//console.log(anotherString);
+const newString = "    shreya    "
+//console.log(newString);
+//console.log(newString.trim());
+
+
+const url = "https://shreya.com/shreya%20kedia"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('shreya'))
+
+
+
+
+
+ 
+
+
+
+
